7c2a73c87358724ae04e4988dae70e3fe82e6342
Nando Cova <nandocova@gmail.com>
2022-07-19 11:00:59 -0500

    Solve 07_tempCoversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  let output = (temp - 32) * (5/9);
+  return Math.round((output + Number.EPSILON) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  let output = ((temp * (9/5)) + 32)
+  return Math.round((output + Number.EPSILON) * 10) / 10;
 };
 
 // Do not edit below this line
