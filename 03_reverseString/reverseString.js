e262a408b4b8df920a3493c7d791a306cfde5014
Nando Cova <nandocova@gmail.com>
2022-07-19 09:56:56 -0500

    Solve 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split("");
+    array.reverse();
+    let output = "";
+    for (i = 0; i < array.length; i++) {
+        output = output + array[i];
+    }
+    return output;
 };
 
 // Do not edit below this line
