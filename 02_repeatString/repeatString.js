10d10583f13d80d828c40d0a66aed8046c67f3f5
Nando Cova <nandocova@gmail.com>
2022-07-19 09:44:04 -0500

    Solve 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let output = string;    
+    if (num === 1) {
+        return string;
+    } else if (num > 1) {
+        for (let i = num; i > 1; i--) {
+            output = output + string;
+        }
+    } else if (num === 0) {
+        output = '';
+    } else if (num < 0) {
+        output = "ERROR";
+    }
 
+    return output;
 };
 
 // Do not edit below this line
