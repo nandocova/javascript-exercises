e0a6496f1792ae722fa2c3c0c66681aeeb093459
Nando Cova <nandocova@gmail.com>
2022-07-19 10:39:52 -0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0) {
+        return "ERROR";
+    }
 
+    if (typeof start != "number" || typeof end != "number"){
+        return "ERROR";
+    }
+
+    let sum = 0;
+    if (start < end) {
+        for (i = start; i <= end; i++) {
+            sum = sum + i;
+        }
+    } else {
+        for (i = end; i <= start; i++) {
+            sum = sum + i;
+        }
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
